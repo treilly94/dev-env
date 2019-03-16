13ca066db8b7b87c578531b77fe7d8c822ab8cf9
Tom Reilly <treilly94@live.com>
2019-03-16 17:00:06 +0000

    added access tf

---
+++ access.tf
@@ -0,0 +1,77 @@
+resource "aws_subnet" "access_subnet" {
+  vpc_id                  = "${aws_vpc.dev_vpc.id}"
+  cidr_block              = "${local.access_cidr}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name          = "${local.env}-subnet"
+    ResourceGroup = "${local.env}"
+  }
+}
+
+resource "aws_route_table_association" "access_route_table_association" {
+  subnet_id      = "${aws_subnet.access_subnet.id}"
+  route_table_id = "${aws_route_table.dev_route_table.id}"
+}
+
+resource "aws_security_group" "access_sg" {
+  name        = "access_security_group"
+  description = "Security rules for the access vms"
+  vpc_id      = "${aws_vpc.dev_vpc.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internal
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${local.vpc_cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${local.vpc_cidr}"]
+  }
+
+  tags = {
+    Name          = "access-security-group"
+    ResourceGroup = "${local.env}"
+  }
+}
+
+resource "aws_instance" "access_vm" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "${local.default_vm_size}"
+  key_name      = "dev-keypair"
+
+  subnet_id       = "${aws_subnet.access_subnet.id}"
+  security_groups = ["${aws_security_group.access_sg.id}"]
+  depends_on      = ["aws_internet_gateway.gw"]
+
+  root_block_device = {
+    volume_type           = "standard"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name          = "access-vm"
+    ResourceGroup = "${local.env}"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "admin"
+}
+
+locals {
+  env             = "Dev"
+  default_vm_size = "t3.nano"
+  vpc_cidr        = "10.0.0.0/16"
+  access_cidr     = "${cidrsubnet(local.vpc_cidr,4,3)}" # 20
+  build_cidr      = "${cidrsubnet(local.vpc_cidr,4,4)}" # 20
+}
+
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["aws-marketplace"]
+
+  filter {
+    name   = "product-code"
+    values = ["aw0evgkw8e5c1q413zgy5pjce"]
+  }
+}
+
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "${local.vpc_cidr}"
+
+  tags = {
+    Name          = "${local.env}-vpc"
+    ResourceGroup = "${local.env}"
+  }
+}
+
+# For internet access
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.dev_vpc.id}"
+
+  tags = {
+    Name          = "${local.env}-gateway"
+    ResourceGroup = "${local.env}"
+  }
+}
+
+resource "aws_route_table" "dev_route_table" {
+  vpc_id = "${aws_vpc.dev_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name          = "${local.env}-route-table"
+    ResourceGroup = "${local.env}"
+  }
+}
