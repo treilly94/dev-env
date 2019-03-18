cfa6ea9e48d296883db7f81a14f84a83d46c7220
Tom Reilly <treilly94@live.com>
2019-03-18 20:10:00 +0000

    Added script for gitlab

---
+++ scripts/gitlab.sh
@@ -0,0 +1,17 @@
+export GITLAB_CONFIG=gitlab_config
+export GITLAB_LOGS=gitlab_logs
+export GITLAB_DATA=gitlab_data
+
+sudo docker volume create --name $GITLAB_CONFIG
+sudo docker volume create --name $GITLAB_LOGS
+sudo docker volume create --name $GITLAB_DATA
+
+sudo docker run --detach \
+  --hostname gitlab.example.com \
+  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --name gitlab \
+  --restart unless-stopped \
+  --volume $GITLAB_CONFIG:/etc/gitlab \
+  --volume $GITLAB_LOGS:/var/log/gitlab \
+  --volume $GITLAB_DATA:/var/opt/gitlab \
+  gitlab/gitlab-ce:latest
